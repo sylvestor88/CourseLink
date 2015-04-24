fd32fbdd144d670c0c310e56649bed3d099742d3
sylvestor88 <sylvestor.george@sjsu.edu>
2015-04-23 19:06:21 -0700

    CMPE 273

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,18 +1,19 @@
-package courseLink;
+package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
+ 
+ 
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan
 public class Application {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SpringApplication.run(Application.class, args);
-	}
-
+	
+    public static void main(String[] args) {
+    	
+        SpringApplication.run(Application.class);
+    }
+    
 }
---
+++ src/main/java/hello/SocialConfig.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.support.ConnectionFactoryRegistry;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SocialConfig {
+	
+	@Bean
+    public ConnectionFactoryLocator connectionFactoryLocator() {
+        ConnectionFactoryRegistry registry = new ConnectionFactoryRegistry();
+       // registry.addConnectionFactory(new LinkedInConnectionFactory(
+         //       .getProperty("linkedin.consumerKey"),
+         //       environment.getProperty("linkedin.consumerSecret")));
+        return registry;
+    }
+	
+}
