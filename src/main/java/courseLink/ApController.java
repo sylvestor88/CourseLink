d13d11adf25729c607c20ddaf5534f8d309e7956
sylvestor88 <sylvestor.george@sjsu.edu>
2015-05-10 00:05:49 -0700

    CMPE 273

--- src/main/java/courseLink/ApController.java
+++ src/main/java/courseLink/ApController.java
@@ -3,7 +3,9 @@ package courseLink;
 import javax.inject.Inject;
 
 import org.springframework.social.linkedin.api.LinkedIn;
+import org.springframework.social.linkedin.api.LinkedInProfile;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,4 +23,21 @@ public class ApController {
     String login() {
         return "login";
     }
+	
+	@RequestMapping("/connections")
+	String connected(Model model){
+		
+		LinkedInProfile profile = linkedIn.profileOperations().getUserProfileFull();
+		model.addAttribute("FirstName", profile.getFirstName());
+		model.addAttribute("LastName", profile.getLastName());
+		model.addAttribute("ProfilePictureUrl", profile.getProfilePictureUrl());
+		//model.addAttribute("Summary", profile.getSummary());
+		//model.addAttribute("Industry", profile.getIndustry());
+		model.addAttribute("Email", profile.getEmailAddress());
+		model.addAttribute("Skills", linkedIn.profileOperations().getUserProfileFull().getSkills());
+		
+		//System.out.println(profile.getProfilePictureUrl());
+		
+		return "connect/connections";
+	}
 }	
