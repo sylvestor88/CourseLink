24955123c7439a8791a5ccc3cc16e7005bc0ba31
vaibhavtupe <tvaibhav15@gmail.com>
2015-05-10 12:58:55 -0700

    coursera api consumption integrated

--- src/main/java/com/project/dto/Sessions.java
+++ src/main/java/com/project/dto/Sessions.java
@@ -1,5 +1,8 @@
 package com.project.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Sessions {
 	
 	int id;
--- src/main/java/courseLink/ApController.java
+++ src/main/java/courseLink/ApController.java
@@ -1,15 +1,26 @@
 package courseLink;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.social.linkedin.api.LinkedIn;
 import org.springframework.social.linkedin.api.LinkedInProfile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.project.dto.CourseInfoBySkill;
+import com.project.implementation.CourseraOperations;
 
 @Controller
 public class ApController {
+	
+	CourseraOperations cop=new CourseraOperations();
 	
 	private LinkedIn linkedIn;
 	
@@ -39,5 +50,15 @@ public class ApController {
 		//System.out.println(profile.getProfilePictureUrl());
 		
 		return "connect/connections";
+	}
+	
+	@RequestMapping(value="/getcoursesbylinkedinskills/{skill}", method=RequestMethod.GET)
+	public ResponseEntity<List<CourseInfoBySkill>> getCourseByLinkedIn(@PathVariable String skill){
+		
+		//String skill="java";
+		List<CourseInfoBySkill> courses=cop.getCoursesBySkills(skill);
+	
+		return new ResponseEntity<List<CourseInfoBySkill>>(courses, HttpStatus.OK);
+		
 	}
 }	
