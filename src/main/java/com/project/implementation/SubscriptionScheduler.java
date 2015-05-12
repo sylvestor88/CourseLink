3d997f41100e5e92faaf69434d419b11897bd46b
vaibhavtupe <tvaibhav15@gmail.com>
2015-05-12 15:01:53 -0700

    Subscription scheduler functionality added

--- src/main/java/com/project/implementation/SubscriptionScheduler.java
+++ src/main/java/com/project/implementation/SubscriptionScheduler.java
@@ -1,7 +1,11 @@
 package com.project.implementation;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,52 +20,92 @@ import courseLink.UserRepository;
 
 @Component
 public class SubscriptionScheduler {
-	
-	
+
+
 	@Autowired
 	EmailNotification  emailNotification;
-	
+
 	@Autowired
-    private CourseRepository courseRepo;
-	
+	private CourseRepository courseRepo;
+
 	@Autowired
 	private UserRepository userRepo;
-	
+
+
 	@Scheduled(fixedRate = 5000)
 	public void sendSubscribedCoursesInfo(){
-		
-		
+
+
+		TimeZone timeZone = TimeZone.getTimeZone("UTC");
+		SimpleDateFormat ft = new SimpleDateFormat ("MM-dd-yyyy");
+		ft.setTimeZone(timeZone);
+		String currentDateString=ft.format((new Date()));
+		Date currentDate=null;
+		try {
+			currentDate = ft.parse(currentDateString);
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
 		List<UserInfo> users=new ArrayList<UserInfo>();
-		
+
 		users=userRepo.findAll();
-		
-		for(UserInfo user:users){
-			
+
+		for(UserInfo user:users){   //user
+
+			String subject="course subscription !!!";
+			StringBuilder msgBody=new StringBuilder(null);
+          
 			
 			String[] skills=user.getSkillChoice();
-			
-			for(String skill:skills){
-				
+
+			for(String skill:skills){  // skill
+
 				List<CourseInfoBySkill> courses=courseRepo.getCoursesBySkill(skill);
-				
-				for(CourseInfoBySkill course:courses){
+
+				for(CourseInfoBySkill course:courses){  // course
+
+
+					String courseStartDateString=course.getSession_start();
+
+					Date courseStartDate=null;
+					try {
+						courseStartDate = ft.parse(courseStartDateString);
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					if(currentDate.compareTo(courseStartDate)<0){
+
+						//in milliseconds
+						long diff = courseStartDate.getTime() - currentDate.getTime(); 
+
+						long diffDays = diff / (24 * 60 * 60 * 1000);
+
+						if(diffDays<6){
+
+							msgBody.append("course name : "+ course.getName()+" course start date : "+course.getSession_start());
 					
-					
-					String couseStartDate=course.getSession_start();
-					
-				}
+						}
+
+					}
+
+				}// skill
+
 				
-				
-				
-				
-			}
-			
+			}// user
+
+			// if msgBody not null then only send the mail
+			if(msgBody!=null)
+			emailNotification.sendEmailonSubscription(user.getEmailId(), user.getUsername(),subject,msgBody);
 		}
-		
-		
-		emailNotification.sendEmailonSubscription("subscribermailid@gmail.com", "subscriber name");
-		
-		
+
+
+
 	}
+	
+	
 
 }
